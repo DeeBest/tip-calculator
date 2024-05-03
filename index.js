c164cb01fd7c640446d3f5b9e75c77924f7e239c
DeeBest <simphiwedladla8@gmail.com>
2024-05-03 20:12:48 +0200

    created a function for calculating the total base on the inputs

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const button = document.querySelector('button');
+const totalOutput = document.getElementById('total-output');
+
+button.addEventListener('click', () => {
+  const billAmount = +document.getElementById('bill-amount').value;
+  const tipPercentage = +document.getElementById('tip-percentage').value;
+
+  if (billAmount > 0 && tipPercentage > 0 && tipPercentage < 100) {
+    let total = billAmount * (tipPercentage / 100) + billAmount;
+    console.log(total);
+    totalOutput.textContent = `Total Bill: $${total.toFixed(2)}`;
+  } else alert('Please enter valid amount.');
+});
